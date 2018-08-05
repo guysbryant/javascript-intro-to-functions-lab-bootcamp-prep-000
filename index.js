72732b263a31de6bdb7c84a6a85b48aca0804740
Guy Bryant <guy.s.bryant@gmail.com>
2018-08-05 21:13:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ function sayHiToGrandma(string){
   } else if (string === string.toUpperCase){
     return "YES INDEED!"
   } else {
-    return "I love you, Grandma."
+    return "I love you, too."
   }
 }
